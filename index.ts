49477382800d80dded985e974fee112ff016dc5a
Yohanes <yohanesmulugeta21@gmail.com>
2023-03-28 22:07:16 +0300

    define ToDo wth interface command.

--- index.ts
+++ index.ts
@@ -13,10 +13,14 @@ axios.get(url).then((res) => {
   const todo = res.data as ToDo;
   const id = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
+  const completed = todo.completed;
 
-  console.log(`
-  The todo with ID: ${id}
-  Title: ${title}
-  Is it finished? ${finished}`);
+  logTodo(id, title, completed);
 });
+
+const logTodo = function (id, title, completed) {
+  console.log(`
+    The todo with ID: ${id}
+    Title: ${title}
+    Is it finished? ${completed}`);
+};
