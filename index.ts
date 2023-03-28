1b9621ff39bf3d3bcf10df26c0dafe5ee0e9892f
Yohanes <yohanesmulugeta21@gmail.com>
2023-03-28 22:04:55 +0300

    define ToDo wth interface command.

---
+++ index.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+// interfaces are use to define the structure of an object
+interface ToDo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((res) => {
+  const todo = res.data as ToDo;
+  const id = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  console.log(`
+  The todo with ID: ${id}
+  Title: ${title}
+  Is it finished? ${finished}`);
+});
